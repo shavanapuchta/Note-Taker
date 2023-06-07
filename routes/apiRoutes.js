bd1025093e09fa452f5680017e0c2aa3d4239e44
shavanapuchta <119765380+shavanapuchta@users.noreply.github.com>
2023-06-06 20:20:16 -0700

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,58 +1,29 @@
-// const notesData = require("../data/notesData");
-const fs = require("fs");
-const util = require("util");
-const app = require("express").Router();
-const writeFileAsync = util.promisify(fs.writeFile);
-const readFileAsync = util.promisify(fs.readFile);
-var notesData;
+const router = require('express').Router();
 
-module.exports = function(app) {
+const saveData = require('../db/saveData');
+
 // GET request
-app.get("/notes", (req, res) => {
-  readFileAsync("db/db.json", "utf8").then(function (data) {
-    notesData = JSON.parse(data);
-    res.json(notesData);
-  });
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
 });
 
-// POST  request
-app.post("/notes", (req, res) => {
-  readFileAsync("db/db.json", "utf8").then(function (data) {
-    
-    notesData = JSON.parse(data);
-
-    let newNote = req.body;
-    let currentID = notesData.length;
-
-    newNote.id = currentID + 1;
-    
-    notesData.push(newNote);
-
-    notesData = JSON.stringify(notesData);
-
-    writeFileAsync("db/db.json", notesData).then(function (data) {
-      console.log("Note has been added.");
-    });
-    res.json(notesData);
-  });
+// POST request
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
 });
 
 // DELETE request
-app.delete("/notes/:id", (req, res) => {
-  let selID = parseInt(req.params.id);
- 
-  for (let i = 0; i < notesData.length; i++) {
-    if (selID === notesData[i].id) {
-      notesData.splice(i, 1);
-      let noteJSON = JSON.stringify(notesData, null, 2);
-
-      writeFileAsync("db/db.json", noteJSON).then(function () {
-        console.log("Note has been deleted.");
-      });
-    }
-  }
-  res.json(notesData);
+router.delete('/notes/:id', function (req, res) {
+    saveData
+        .deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
 });
 
-
-module.exports = app;}
+module.exports = router;
